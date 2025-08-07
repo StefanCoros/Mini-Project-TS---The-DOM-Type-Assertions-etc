65d07b884a1dad2696f165504d4bb1e08ad5f836
Stefan <coros.andrei.stefan88@gmail.com>
2025-08-07 16:21:14 +0300

    todo list ui wip

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,24 @@
 "use strict";
 var btn = document.getElementById("btn");
 var input = document.getElementById("todoinput");
-btn.addEventListener("click", function () {
-    alert(input.value);
+var form = document.getElementById("todoform");
+var list = document.getElementById("todolist");
+var todos = [];
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    input.value = "";
 });
+var createTodo = function (todo) {
+    var newLi = document.createElement("li");
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newLi.append(todo.text);
+    newLi.append(checkbox);
+    list.append(newLi);
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
-const btn = document.getElementById("btn")!;
-const input = document.getElementById("todoinput")! as HTMLInputElement;
+interface Todo {
+  text: string;
+  completed: boolean;
+}
 
-btn.addEventListener("click", () => {
-  alert(input.value);
+const btn = document.getElementById("btn")! as HTMLButtonElement;
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.getElementById("todoform");
+const list = document.getElementById("todolist")!;
+
+const todos: Todo[] = [];
+
+form?.addEventListener("submit", (e: SubmitEvent) => {
+  e.preventDefault();
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+  input.value = "";
 });
+
+const createTodo = (todo: Todo) => {
+  const newLi = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  newLi.append(todo.text);
+  newLi.append(checkbox);
+  list.append(newLi);
+};
